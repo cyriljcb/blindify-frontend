80484740096b0e93c51f7c87fbdff0e05b4b7b22
CyrilJcb <jacobcyril042@gmail.com>
2024-12-26 11:12:27 +0100

    initial commit with module

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from '../app-routing.module';
+import { LoginComponent } from './components/login/login.component';
+
+@NgModule({
+  declarations: [
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  exports:[
+    LoginComponent
+  ],
+  
+})
+export class AuthModule { }
